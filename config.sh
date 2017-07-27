985df93d309c3713471e322d30b10095a1427a5f
Sergio Pascual <sergiopr@fis.ucm.es>
2017-07-27 18:45:51 +0200

    Adding yum commands

--- config.sh
+++ config.sh
@@ -1,14 +1,19 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
-echo "CONFFFFF"
+echo "We are in conf.sh"
 
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    echo "OEEEHEHEHEEHEHE"
-    build_libpng
-    build_freetype
+    echo "We are in pre_build"
+    if [ -n "$IS_OSX" ]; then
+       echo "We are in IS_OSX"
+    else
+       echo "We are NOT in IS_OSX"
+       sudo yum install pkgconfig libpng-devel
+    fi
+
 }
 
 function run_tests {
