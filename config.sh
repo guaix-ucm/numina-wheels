8a4890f14a8c91bb095be7b6efda4df5c474a157
Sergio Pascual <sergiopr@fis.ucm.es>
2017-07-27 18:34:32 +0200

    Please work

--- config.sh
+++ config.sh
@@ -1,17 +1,19 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
+echo "CONFFFFF"
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     echo "OEEEHEHEHEEHEHE"
-    if [ -z "$IS_OSX" ]; then
-        sudo apt-get pkg-config libpng-dev
-    fi
+    build_libpng
+    build_freetype
 }
 
 function run_tests {
     # Runs tests on installed distribution from an empty directory
+    echo "NOT TESTING"
     python --version
     #python -c 'import sys; import numina; sys.exit(numina.test())'
 }
